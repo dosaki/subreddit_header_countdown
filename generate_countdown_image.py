66bb184f7b0e167c74daa2f6679e42c9cea3ec31
Tiago 'Dosaki' Correia <lord.g.bug@gmail.com>
2016-04-01 00:08:49 +0100

    Making it more configurable. Allowing text to be added. improving tests.

--- generate_countdown_image.py
+++ generate_countdown_image.py
@@ -7,11 +7,12 @@ def send_header(img, reddit, subreddit):
     img.save("header_cd.png")
     reddit.upload_image(subreddit, "header_cd.png", name=None, header=True)
 
-def apply_text_on_image(image, text, font, pos):
+def apply_text_on_image(image, text, font_path, pos, colour):
+    rgb = parse_rgb_colour(colour)
     img = Image.open(image)
-    font = ImageFont.truetype(font, 25)
+    image_font = ImageFont.truetype(font_path, 25)
     draw = ImageDraw.Draw(img)
-    draw.text((int(pos['x']), int(pos['y'])), text, (250, 250, 255), font=font)
+    draw.text((pos['x'], pos['y']), text, fill=(rgb['r'], rgb['g'], rgb['b']), font=image_font)
     draw = ImageDraw.Draw(img)
     return img
 
@@ -38,28 +39,75 @@ def remaining_time(target):
         'days': days
     }
 
-def format_time_simpler(timediff):
+def parse_rgb_colour(rgb_string):
+    rgb_array = rgb_string.split(',')
+    return {
+        'r': int(rgb_array[0]),
+        'g': int(rgb_array[1]),
+        'b': int(rgb_array[2])
+    }
+
+def get_reddit_properties(conf):
+    return {
+        'username': conf.get('reddit', 'username'),
+        'password': conf.get('reddit', 'password'),
+        'subreddit': conf.get('reddit', 'subreddit')
+    }
+
+def get_image_properties(conf):
+    return {
+        'location': conf.get('image', 'image')
+    }
+
+def get_countdown_properties(conf):
+    return {
+        'target': conf.get('countdown', 'target'),
+        'enabled': conf.get('countdown', 'enable'),
+        'pos': {
+            'x': int(conf.get('countdown', 'posx')),
+            'y': int(conf.get('countdown', 'posy'))
+        },
+        'colour': conf.get('countdown', 'rgb'),
+        'font': conf.get('countdown', 'font')
+    }
+
+def get_text_properties(conf):
+    return {
+        'text': conf.get('text', 'text'),
+        'enabled': conf.get('text', 'enable'),
+        'pos': {
+            'x': int(conf.get('text', 'posx')),
+            'y': int(conf.get('text', 'posy'))
+        },
+        'colour': conf.get('text', 'rgb'),
+        'font': conf.get('text', 'font')
+    }
+
+def set_up_reddit(username, password):
+    r = Reddit(user_agent='linux:net.dosaki.subreddit_header_countdown:0.0.2 (by /u/dosaki)')
+    r.login(username, password)
+    return r
+
+def format_time_simple(timediff):
     return "%s days, %sh %sm" % (timediff['days'], timediff['hours'], timediff['minutes'])
 
 if __name__ == '__main__':
     conf = SafeConfigParser()
     conf.read('settings.cfg')
 
-    username = conf.get('reddit', 'username')
-    password = conf.get('reddit', 'password')
-    subreddit = conf.get('reddit', 'subreddit')
-    target = conf.get('image', 'target')
-    image_location = conf.get('image', 'image')
-    font = conf.get('image', 'font')
-    posx = conf.get('image', 'posx')
-    posy = conf.get('image', 'posy')
+    reddit_cfg = get_reddit_properties(conf)
+    text_cfg = get_text_properties(conf)
+    countdown_cfg = get_countdown_properties(conf)
+    image_cfg = get_image_properties(conf)
 
+    r = set_up_reddit(reddit_cfg['username'], reddit_cfg['password'])
 
-    r = Reddit(user_agent='linux:net.dosaki.subreddit_header_countdown:0.0.1 (by /u/dosaki)')
-    r.login(username, password)
+    if countdown_cfg['enabled'] == "true":
+        target_datetime = datetime.strptime(countdown_cfg['target'], '%Y %m %d %H:%M:%S')
+        timediff = remaining_time(target_datetime)
+        image = apply_text_on_image(image_cfg['location'], format_time_simple(timediff), countdown_cfg['font'], countdown_cfg['pos'], countdown_cfg['colour'])
 
-    target_datetime = datetime.strptime(target, '%Y %m %d %H:%M:%S')
-    timediff = remaining_time(target_datetime)
+    if text_cfg['enabled'] == "true":
+        image = apply_text_on_image(image_cfg['location'], text_cfg['text'], text_cfg['font'], text_cfg['pos'], countdown_cfg['colour'])
 
-    image = apply_text_on_image(image_location, format_time_simpler(timediff), font, {'x':posx, 'y':posy})
     send_header(image, r, subreddit)
--- test/test_makes_image.py
+++ test/test_makes_image.py
@@ -5,32 +5,57 @@ from datetime import datetime
 
 from ConfigParser import SafeConfigParser
 sys.path.insert(0, os.path.dirname(os.path.abspath(__file__))+'/../')
-from generate_countdown_image import apply_text_on_image, format_time_simpler, remaining_time
+
+import generate_countdown_image as gci
 
 class TestCsvToJson(unittest.TestCase):
 
     def test_control(self):
         self.assertTrue(True, 'Control test successful!')
 
-    def test_makes_image(self):
+    def test_get_reddit_properties(self):
+        conf = SafeConfigParser()
+        conf.read('test/settings.cfg')
+        props = gci.get_reddit_properties(conf)
+        assert props['username'] == 'user'
+        assert props['password'] == 'password'
+        assert props['subreddit'] == 'subreddit'
+
+    def test_get_image_properties(self):
+        conf = SafeConfigParser()
+        conf.read('test/settings.cfg')
+        props = gci.get_image_properties(conf)
+        assert props['location'] == './header.png'
+
+    def test_get_text_properties(self):
+        conf = SafeConfigParser()
+        conf.read('test/settings.cfg')
+        props = gci.get_text_properties(conf)
+
+        assert props['enabled'] == 'true'
+        assert props['text'] == 'Hello World! :D'
+        assert props['pos'] == {
+                'x': 535,
+                'y': 50,
+            }
+        assert props['colour'] == '250,250,255'
+        assert props['font'] == './Montserrat-Regular.otf'
+
+    def test_makes_image_countdown(self):
         conf = SafeConfigParser()
         conf.read('test/settings.cfg')
 
-        username = conf.get('reddit', 'username')
-        password = conf.get('reddit', 'password')
-        subreddit = conf.get('reddit', 'subreddit')
-        target = conf.get('image', 'target')
-        image_location = 'test/header.png'
-        font = conf.get('image', 'font')
-        posx = conf.get('image', 'posx')
-        posy = conf.get('image', 'posy')
+        reddit_cfg = gci.get_reddit_properties(conf)
+        image_cfg = gci.get_image_properties(conf)
+        countdown_cfg = gci.get_countdown_properties(conf)
 
-        target_datetime = datetime.strptime(target, '%Y %m %d %H:%M:%S')
-        timediff = remaining_time(target_datetime)
+        target_datetime = datetime.strptime(countdown_cfg['target'], '%Y %m %d %H:%M:%S')
+        timediff = gci.remaining_time(target_datetime)
+        image = gci.apply_text_on_image(image_cfg['location'], gci.format_time_simple(timediff), countdown_cfg['font'], countdown_cfg['pos'], countdown_cfg['colour'])
+        image.save('test/result.png')
 
-        result = apply_text_on_image(image_location, format_time_simpler(timediff), font, {'x':posx, 'y':posy})
-        result.save('test/result.png')
+        assert os.path.exists('test/result.png')
+        os.remove('test/result.png')
 
 if __name__ == '__main__':
     unittest.main()
-    
